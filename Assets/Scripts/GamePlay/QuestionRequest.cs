82eb48ea91afacfd0ba2fe5f9ac4149c7360f219
ZikkeyLS <hisaha21@yandex.ru>
2023-06-29 13:09:45 +0300

    Full translation

--- Assets/Scripts/GamePlay/QuestionRequest.cs
+++ Assets/Scripts/GamePlay/QuestionRequest.cs
@@ -7,6 +7,7 @@ public class QuestionRequest : MonoBehaviour
     [SerializeField] private TMP_Text _questionNumber;
     [SerializeField] private TMP_Text _questionText;
     [SerializeField] private Button[] _answers;
+    [SerializeField] private VariativeText _questionNumberText;
     private TMP_Text[] _answersText;
 
     public void MakeRequest(int id, Question question)
@@ -18,10 +19,10 @@ public class QuestionRequest : MonoBehaviour
                 _answersText[i] = _answers[i].GetComponentInChildren<TMP_Text>();
         }
 
-        _questionNumber.text = "Вопрос номер " + id;
-        _questionText.text = question.Text;
+        _questionNumber.text = $"{_questionNumberText.GetText(YandexLanguage.Language)} {id}";
+        _questionText.text = question.Text.GetText(YandexLanguage.Language);
 
         for (int i = 0; i < _answers.Length; i++)
-            _answersText[i].text = question.Answers[i].Text;
+            _answersText[i].text = question.Answers[i].Text.GetText(YandexLanguage.Language);
     }
 }
--- Assets/Scripts/Scriptable/Question.cs
+++ Assets/Scripts/Scriptable/Question.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 [Serializable]
 public class Answer
 {
-    public string Text;
+    public VariativeText Text;
     public Character Character;
 }
 
 [CreateAssetMenu(fileName = "Question", menuName = "Game/Question")]
 public class Question : ScriptableObject
 {
-    [Multiline] public string Text;
+    public VariativeText Text;
     public Answer[] Answers = new Answer[4];
 }
